c4f140509af552f1a6464c8c6b8e7c64f95fc1a0
hisa11 <hsiahisa1120@outlook.jp>
2024-09-04 17:37:37 +0900

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,39 @@
 BufferedSerial pc(USBTX, USBRX, 250000); // パソコンとのシリアル通信
 CAN can(PA_11, PA_12, (int)1e6);
 uint8_t DATA[8] = {};
-PID pidControllerRight(0.002, 0.00, 0.005, 0.00001, 501, 0.01);
+PID pidControllerRight(0.001, 0.00, 0.00, 0.0000, 501, 0.01);
 int targetSpeed = 0;
 int currentSpeed = 0;
+int16_t outputpicInt16 = 0;
+
+void can_thread()
+{
+    while (1)
+    {
+        CANMessage msg1;
+        if (can.read(msg1) && msg1.id == 0x201)
+        {
+            currentSpeed = (msg1.data[2] << 8) | msg1.data[3];
+        }
+
+        // PID制御の計算
+        float output = pidControllerRight.calculate(targetSpeed, currentSpeed);
+        outputpicInt16 = static_cast<int16_t>(output);
+
+        // デバッグ用の出力を追加
+        char buffer[80];
+        sprintf(buffer, "Target: %d, Speed: %d, Output: %f\n", targetSpeed, currentSpeed, output);
+        pc.write(buffer, strlen(buffer));
+
+        ThisThread::sleep_for(10ms);
+    }
+}
 
 int main()
 {
+    Thread canThread;
+    canThread.start(can_thread);
+
     while (1)
     {
         if (pc.readable())
@@ -26,25 +53,12 @@ int main()
             }
         }
 
-        CANMessage msg1, msg2;
-        if (can.read(msg1) && msg1.id == 0x201)
-        {
-            currentSpeed = (msg1.data[2] << 8) | msg1.data[3];
-        }
-
-        // PID制御の計算
-        float output = pidControllerRight.calculate(targetSpeed, currentSpeed);
-        int16_t outputpicInt16 = static_cast<int16_t>(output);
+        // CANメッセージの送信
         DATA[0] = outputpicInt16 >> 8;   // MSB
         DATA[1] = outputpicInt16 & 0xFF; // LSB
         CANMessage msg0(0x200, DATA, 8);
-
-        // CANメッセージの送信
         can.write(msg0);
 
-        // デバッグ用の出力を追加
-        char buffer[80];
-        sprintf(buffer, "Target: %d, Speed: %d, Output: %f\n", targetSpeed, currentSpeed, output);
-        pc.write(buffer, strlen(buffer));
+        ThisThread::sleep_for(10ms);
     }
 }
